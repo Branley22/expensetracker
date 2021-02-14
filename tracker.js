1acbf20ce9a5155754025b22829edb6f3bfb608b
Branley22 <bminaya2@gmail.com>
2021-02-13 21:17:15 -0500

    js code commit

--- tracker.js
+++ tracker.js
@@ -1,8 +1,29 @@
-function doMath() {
-    var name = document.querySelector("name").value;
-    var datepicker = document.querySelector("datepicker").value;
-    var amount = document.querySelector("amount").value;
+ var entry = document.getElementById("entry");
+ entry.addEventListener("click", displayDetails);
 
-    let sum = parseInt(name) + parseInt(datepicker) + parseInt(amount);
-    document.write(sum);
-}
+    var row = 1;
+
+    function displayDetails() {
+        var name = document.getElementById("name").value;
+        var date = document.getElementById("date").value;
+        var amount = document.getElementById("amount").value;
+        
+        if(!name || !date || !amount) {
+            alert("Fill all boxes");
+            return;
+        }
+        var display = document.getElementById("display");
+
+        var newRow = display.insertRow(row);
+        
+        var cell1 = newRow.insertCell(0);
+        var cell2 = newRow.insertCell(1);
+        var cell3 = newRow.insertCell(2);
+
+        cell1.innerHTML = name;
+        cell2.innerHTML = date;
+        cell3.innerHTML = amount;
+
+        row++;
+                
+    }

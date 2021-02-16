8932bb2963cfd4f842b5db5a8f3c52a1197a2daf
Branley22 <bminaya2@gmail.com>
2021-02-16 13:19:46 -0500

    js commit

--- tracker.js
+++ tracker.js
@@ -1,29 +1,33 @@
- var entry = document.getElementById("entry");
+ const entry = document.getElementById("entry");
  entry.addEventListener("click", displayDetails);
 
     var row = 1;
 
     function displayDetails() {
-        var name = document.getElementById("name").value;
-        var date = document.getElementById("date").value;
-        var amount = document.getElementById("amount").value;
+        const name = document.getElementById("name").value;
+        const date = document.getElementById("date").value;
+        const amount = document.getElementById("amount").value;
+        
         
         if(!name || !date || !amount) {
             alert("Fill all boxes");
             return;
         }
-        var display = document.getElementById("display");
+        const display = document.getElementById("display");
 
-        var newRow = display.insertRow(row);
+        const newRow = display.insertRow(row);
         
-        var cell1 = newRow.insertCell(0);
-        var cell2 = newRow.insertCell(1);
-        var cell3 = newRow.insertCell(2);
+        const cell1 = newRow.insertCell(0); 
+        const cell2 = newRow.insertCell(1);
+        const cell3 = newRow.insertCell(2);
 
         cell1.innerHTML = name;
         cell2.innerHTML = date;
         cell3.innerHTML = amount;
 
         row++;
-                
+    }
+
+    function myDeleteFunction() {
+        const display = document.getElementById("display").deleteRow(0);
     }
